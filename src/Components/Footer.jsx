90708a2d22339677994711d2636f910cd1ab2b15
pranava112 <pranavamanjunath@gmail.com>
2025-06-24 23:19:44 +0530

    code on copy rights

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,3 +1,51 @@
+// import './Footer.css';
+
+// import { Col, Container, Row } from 'react-bootstrap';
+// import { FaFacebook, FaInstagram, FaPhone, FaTwitter } from 'react-icons/fa';
+
+// import { CiLinkedin } from 'react-icons/ci';
+// import React from 'react';
+
+// const Footer = () => {
+//   return (
+//     <footer className="footer bg-dark text-white py-4 mt-5">
+//       <Container>
+//         <Row className="align-items-center text-center text-md-start">
+//           <Col md={6} className="mb-3 mb-md-0">
+//             <div className="phone-wrapper">
+//               <FaPhone className="me-2" />
+//               <span className="phone">+91-9999999999</span>
+//             </div>
+//           </Col>
+
+//           <Col >
+//            <p>&copy; 2025 Your Company. All rights reserved.</p>
+//           </Col>
+
+//           <Col md={6} className="text-md-end mb-3 mb-md-0">
+//             <div className="icons">
+//               <a href="#" className="icon me-3"><FaFacebook /></a>
+//               <a href="#" className="icon me-3"><FaInstagram /></a>
+//               <a href="#" className="icon me-3"><FaTwitter /></a>
+//               <a href="#" className="icon"><CiLinkedin /></a>
+//             </div>
+//           </Col>
+//         </Row>
+
+//         <Row className="mt-3">
+//           <Col className="text-center">
+//             <p className="mb-0 developer-credit">
+//               Designed and Developed by V. Pranava Manjunath
+//             </p>
+//           </Col>
+//         </Row>
+//       </Container>
+//     </footer>
+//   );
+// };
+
+// export default Footer;
+
 import './Footer.css';
 
 import { Col, Container, Row } from 'react-bootstrap';
@@ -11,14 +59,22 @@ const Footer = () => {
     <footer className="footer bg-dark text-white py-4 mt-5">
       <Container>
         <Row className="align-items-center text-center text-md-start">
-          <Col md={6} className="mb-3 mb-md-0">
-            <div className="phone-wrapper">
+          
+          {/* Contact Info */}
+          <Col md={4} className="mb-3 mb-md-0">
+            <div className="phone-wrapper d-flex justify-content-center justify-content-md-start align-items-center">
               <FaPhone className="me-2" />
               <span className="phone">+91-9999999999</span>
             </div>
           </Col>
 
-          <Col md={6} className="text-md-end mb-3 mb-md-0">
+          {/* Copyright */}
+          <Col md={4} className="mb-3 mb-md-0 text-center">
+            <p className="mb-0">&copy; 2025 IJMSBC . All rights reserved.</p>
+          </Col>
+
+          {/* Social Icons */}
+          <Col md={4} className="text-center text-md-end">
             <div className="icons">
               <a href="#" className="icon me-3"><FaFacebook /></a>
               <a href="#" className="icon me-3"><FaInstagram /></a>
@@ -26,8 +82,10 @@ const Footer = () => {
               <a href="#" className="icon"><CiLinkedin /></a>
             </div>
           </Col>
+
         </Row>
 
+        {/* Developer Credit */}
         <Row className="mt-3">
           <Col className="text-center">
             <p className="mb-0 developer-credit">
