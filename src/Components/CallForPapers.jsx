67a78bf8ed725eaf82eac7ccc25a96a778ba9c20
pranava manjunath <pranavamanjunath@gmail.com>
2025-09-12 14:10:03 +0530

    updates in call for papers

--- src/Components/CallForPapers.jsx
+++ src/Components/CallForPapers.jsx
@@ -1,17 +1,30 @@
-import { Button } from 'react-bootstrap';
-import React from 'react';
+// import { Button } from 'react-bootstrap';
+// import React from 'react';
 
-const CallButton = () => {
-  const handleCall = () => {
-    window.location.href = "tel:+918143123534";
-    // BsWindowSidebar.location.href="tel:+91"
-  };
+// const CallButton = () => {
+//   const handleCall = () => {
+//     window.location.href = "tel:+918143123534";
+//     // BsWindowSidebar.location.href="tel:+91"
+//   };
 
+//   return (
+//     <Button variant="success" onClick={handleCall}>
+//       📞 Call Now
+//     </Button>
+//   );
+// };
+
+
+// export default CallButton;
+
+import React from 'react'
+
+const CallForPapers = () => {
   return (
-    <Button variant="success" onClick={handleCall}>
-      📞 Call Now
-    </Button>
-  );
-};
+    <div>
+      Call For Papers
+    </div>
+  )
+}
 
-export default CallButton;
+export default CallForPapers
