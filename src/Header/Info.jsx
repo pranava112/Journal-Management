5f839863ecdf5fa4d842fceeddca437d595e96de
pranava manjunath <pranavamanjunath@gmail.com>
2026-01-23 23:42:57 +0530

    changes as of 23/01/2026 by user

--- src/Header/Info.jsx
+++ src/Header/Info.jsx
@@ -5,7 +5,7 @@ const Info = () => {
     <>
       <div className='info-bar  py-1 px-2 '>
         <h3>ISSN No:****_****</h3>
-        <h3>Email : ijmsabc@gmail.com</h3>
+        <h3>Email us : ijmsabc@gmail.com</h3>
       </div>
     </>
   )
